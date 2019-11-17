467eebcb3a9cb709004fe2103be5084e38c7b12e
unknown <mendez.andrewm@gmail.com>
2019-11-17 11:31:21 -0700

    Initial Navbar setup

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navburger").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("navburger").style.width = "0%";
+  }
